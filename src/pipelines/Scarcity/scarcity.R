e02e21cb3f514f4d56971af2d84252bbf7172fd4
diogoburgos <110135811+diogoburgos@users.noreply.github.com>
2023-08-28 16:09:24 -0300

    wip

--- src/pipelines/Scarcity/scarcity.R
+++ src/pipelines/Scarcity/scarcity.R
@@ -4,31 +4,29 @@
 # ecosystem and lifestyle.                                                             #
 #                                                                                      #
 ########################################################################################
-
+source("src/data_wrangling/merge_annotation_metadata/merge_annotation_metadata.R")
 
 # Load packages
 library(tidyverse)
 library(funrar)
 library(Matrix)
+library(vegan)
 
 # Load data
-df <- read_csv("input/genus.general.relative.matrix.csv")
-
+df <- read_csv("input/kraken_biomedb_relative_genera.csv")
 
 # Temporary: remove samples not present in atualized metadata
 meta <- read_csv("input/biome_classification.csv")
 str(meta)
-df <- df %>% filter(samples %in% meta$samples)
+df <- df %>% filter(df$samples %in% meta$samples)
 nrow(df)
 
-# Rename columns
-df <- df %>%
-  rename(
-    life_style = level_1,
-    ecosystem = level_2,
-    habitat = level_3
-  )
-
+#merging metadata and taxonomy df
+df <- merge_annotation_metadata(
+  annotation_df = df,
+  metadata_df = meta,
+  metadata_variables = c("samples", "life_style", "ecosystem", "habitat")
+)
 
 # Create the df with the samples names.
 df_char <- df %>%
@@ -67,9 +65,9 @@ df_clean_character <- df_clean %>%
 # Lets create our matrix using numeric variables
 df_matrix <- as.matrix(df_clean_numeric)
 
+is.numeric(df_matrix)
 # Now we are going to set the rownames of our matrix based on the character
 # variables.
-
 rownames(df_matrix) <- paste0(df_clean_character$samples)
 
 ## Our matrix was created. Let's clear the global environment to free up memory.
@@ -87,15 +85,20 @@ rm(list = setdiff(
 sum(is.na(df_matrix))
 
 sum(df_matrix == 0)
-#sum of non zero values for each row:
+#sum of non zero values for each row (richness per sample):
 rich_persample <- rowSums(df_matrix != 0)
- 
+
+#diversity per sample
+div_persample <- diversity(df_matrix, index = "simpson")
+
+
 ####### Run Scarcity function #############
 
 si_df <- scarcity(df_matrix)
 
 #check if si_df have NA values
 sum(is.na(si_df))
+
 
 si_means <- rowMeans(si_df, na.rm = TRUE) #taking mean si for each sample
 
@@ -113,4 +116,13 @@ si_df <- cbind(si_df[, 1, drop = F], mean_si = si_means, si_df[, -1])
 
 #Put the richness in the dataframe
 si_df <- cbind(si_df[, 1:2, drop = F], richness = rich_persample, si_df[, -c(1:3)])
-position
+
+#Put the diversity in the dataframe
+si_df <- cbind(si_df[, 1:3, drop = F], diversity = div_persample, si_df[, -c(1:4)])
+
+View(head(si_df))
+
+sum(si_df == "Invalid Number")
+
+#order the dataframe by the mean si
+si_df_ord <- si_df[order(si_df$mean_si), ]
