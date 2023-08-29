8986d4d8e0f3f11705bf2e1c78962a1a272422e8
diogoburgos <110135811+diogoburgos@users.noreply.github.com>
2023-08-29 15:24:00 -0300

    runned scarcity and saved scarcty means tbl

--- src/pipelines/Scarcity/scarcity.R
+++ src/pipelines/Scarcity/scarcity.R
@@ -4,13 +4,14 @@
 # ecosystem and lifestyle.                                                             #
 #                                                                                      #
 ########################################################################################
-source("src/data_wrangling/merge_annotation_metadata/merge_annotation_metadata.R")
 
 # Load packages
 library(tidyverse)
 library(funrar)
 library(Matrix)
 library(vegan)
+
+source("src/data_wrangling/merge_annotation_metadata/merge_annotation_metadata.R")
 
 # Load data
 df <- read_csv("input/kraken_biomedb_relative_genera.csv")
@@ -62,29 +63,32 @@ df_clean_numeric <- df_clean %>%
 df_clean_character <- df_clean %>%
   select_if(is.character)
 
+
 # Lets create our matrix using numeric variables
 df_matrix <- as.matrix(df_clean_numeric)
 
 is.numeric(df_matrix)
+
 # Now we are going to set the rownames of our matrix based on the character
 # variables.
 rownames(df_matrix) <- paste0(df_clean_character$samples)
-
-## Our matrix was created. Let's clear the global environment to free up memory.
+is.numeric(df_matrix)
+sum(is.na(df_matrix))
+sum(df_matrix == 0)
+dim(df_matrix)
 
 df_filling <- 1 - sum(df_matrix == 0) / (ncol(df_matrix) * nrow(df_matrix))
 
 df_filling
 
+## Our matrix was created. Let's clear the global environment to free up memory.
 rm(list = setdiff(
   ls(),
   c("df_matrix",   
     "df_char")
 ))
 
-sum(is.na(df_matrix))
-
-sum(df_matrix == 0)
+is.numeric(df_matrix)
 #sum of non zero values for each row (richness per sample):
 rich_persample <- rowSums(df_matrix != 0)
 
@@ -94,35 +98,81 @@ div_persample <- diversity(df_matrix, index = "simpson")
 
 ####### Run Scarcity function #############
 
-si_df <- scarcity(df_matrix)
+#transform the matrix in stack
+df_stack <- matrix_to_stack(df_matrix, 
+                        value_col = "value", 
+                        row_to_col = "samples", 
+                        col_to_col = "genus")
 
+si_stack <- scarcity_stack(df_stack,
+                        sp_col = "genus",
+                        com = "samples",
+                        abund = "value")
+View(head(si_stack))
 #check if si_df have NA values
-sum(is.na(si_df))
+sum(is.na(si_stack))
+
+si_sample_means <- si_stack %>%
+  group_by(samples) %>%
+  summarise(mean_si = mean(Si, na.rm = TRUE))
+
+nrow(si_sample_means)
+
+#puting the richness in the si_sample_means df
+si_sample_means <- cbind(si_sample_means, richness = rich_persample)
+
+#puting the diversity in the si_sample_means df
+si_sample_means <- cbind(si_sample_means, diversity = div_persample)
+
+#removing rownames
+rownames(si_sample_means) <- NULL
+
+#adding habitat, ecosystem and life_style to the si_sample_means df
+si_sample_means <- merge(
+  x = si_sample_means,
+  y = df_char,
+  by = "samples",
+  all.x = TRUE
+)
+
+#reorder columns to be samples, then life_style, then ecosystem, then habitat, then mean_si, then richness, then diversity
+si_sample_means <- si_sample_means[, c(1, 5, 6, 7, 2, 3, 4)]
+
+View(si_sample_means)
+#saving the df
+write_csv(si_sample_means, "input/scarcity_means_persample_29_08_2023.csv")
+
+#saving the stack df
+write_csv(si_stack, "input/scarcity_stack_df_29_08_2023.csv")
 
 
-si_means <- rowMeans(si_df, na.rm = TRUE) #taking mean si for each sample
+########################################################################################
+#Here is the part of the script for calculating scarcity withot using the stack matrix##
+#but it was returning some columns as "Invalid Number", and I don't know how to fix it##
+########################################################################################
+
+#si_df <- scarcity(df_matrix)
+
+#si_means <- rowMeans(si_df, na.rm = TRUE) #taking mean si for each sample
 
 #convert to a dataframe creating a column named samples with the row names
-si_df <- as.data.frame(si_df)
+#si_df <- as.data.frame(si_df)
 
 
 #creating a samples column with the row names
-si_df$samples <- rownames(si_df)  
+#si_df$samples <- rownames(si_df)  
 #putting the samples column in the first 
-si_df <- si_df[, c(ncol(si_df), 1:(ncol(si_df)-1))]
+#si_df <- si_df[, c(ncol(si_df), 1:(ncol(si_df)-1))]
 
 #Put the means in the dataframe
-si_df <- cbind(si_df[, 1, drop = F], mean_si = si_means, si_df[, -1])
+#si_df <- cbind(si_df[, 1, drop = F], mean_si = si_means, si_df[, -1])
 
 #Put the richness in the dataframe
-si_df <- cbind(si_df[, 1:2, drop = F], richness = rich_persample, si_df[, -c(1:3)])
+#si_df <- cbind(si_df[, 1:2, drop = F], richness = rich_persample, si_df[, -c(1:3)])
 
 #Put the diversity in the dataframe
-si_df <- cbind(si_df[, 1:3, drop = F], diversity = div_persample, si_df[, -c(1:4)])
+#si_df <- cbind(si_df[, 1:3, drop = F], diversity = div_persample, si_df[, -c(1:4)])
 
-View(head(si_df))
+#View(head(si_df))
 
-sum(si_df == "Invalid Number")
-
-#order the dataframe by the mean si
-si_df_ord <- si_df[order(si_df$mean_si), ]
+#sum(si_df == "Invalid Number") #this line returns 0 for some reason, but there is cells with this value
