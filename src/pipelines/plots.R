dacc638ba8e3126062627904e3fac514273f0c12
diogoburgos <110135811+diogoburgos@users.noreply.github.com>
2023-08-30 11:24:56 -0300

    wip

--- src/pipelines/plots.R
+++ src/pipelines/plots.R
@@ -8,8 +8,8 @@ source("src/pipelines/plot_functions/draw_barplot.R")
 
 #Colors
 dual_pallete <- c(
-  "host_associated" = "#ffd700",
-  "free_living" = "#00b2ef")
+  "host-associated" = "#ffd700",
+  "free-living" = "#00b2ef")
 pallete <- dual_pallete
 
 axis_text_angle = 45
@@ -40,3 +40,20 @@ ecosystem_plot <- bar_plot(
 ecosystem_plot
 
 ggsave("results/plots/ecosystem_plot.png", width = 15, height = 10, units = "in")
+
+
+#life_style_plot
+life_style_plot <- bar_plot(
+    data = si,
+    x_var = "life_style",
+    y_var = "mean_si",
+    fill_var = "life_style",
+    summarized_data = si_life_style,
+    average_var = "mean_mean_si",
+    error_var = "se_mean_si",
+    fill_pallete = pallete
+)
+
+life_style_plot
+
+ggsave("results/plots/life_style_plot.png", width = 10, height = 15, units = "in")
