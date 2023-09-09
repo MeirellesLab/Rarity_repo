9d307ebd8b3fd831983e074e2c95f6f092f16597
diogoburgos <110135811+diogoburgos@users.noreply.github.com>
2023-09-09 15:13:02 -0300

    wip

--- src/data_wrangling/dw_index_tables.R
+++ src/data_wrangling/dw_index_tables.R
@@ -86,7 +86,7 @@ si_ecosystem <- si  %>%
 
 
 #habitat summarized si means
-si_ecosystem <- si  %>%
+si_habitat <- si  %>%
       group_by(life_style, habitat, ecosystem) %>%
       dplyr::summarise(
         n = dplyr::n(),
@@ -94,5 +94,95 @@ si_ecosystem <- si  %>%
         mean_mean_si = mean(mean_si),
         sd_mean_si = sd(mean_si),
         se_mean_si = sd_mean_si / sqrt(n)) %>%
+      select(-n)
+
+
+#Now with distinctiveness
+di <- read_csv("input/distinctiveness_means_persample_08_09_2023.csv")
+
+di$life_style <- as.factor(di$life_style)
+
+## There is something very strange with this function. But it was the only way
+## I found to reorder the DF based in the life style, ecosystem, then the mean,
+## nested in this order.
+di <- di %>%
+
+  group_by(life_style) %>%
+  mutate(life_style = fct_relevel(life_style, "host-associated", "free-living")) %>%
+  ungroup() %>%
+
+  group_by(ecosystem) %>%
+  mutate(mean = mean(mean_di)) %>%
+  ungroup() %>%
+  mutate(ecosystem = fct_reorder(ecosystem, mean)) %>%
+
+#   group_by(life_style) %>%
+#   mutate(ecosystem = fct_reorder(ecosystem, mean)) %>%
+#   select(-mean) %>%
+
+  group_by(ecosystem, habitat) %>%
+  mutate(mean = mean(mean_di)) %>%
+  ungroup() %>%
+
+  group_by(ecosystem) %>%
+  mutate(habitat = fct_reorder(habitat, mean)) %>%
+  select(-mean)
+
+di$life_style <- as.factor(di$life_style)
+
+di <- di %>% 
+      mutate(
+            ecosystem = case_when(
+            ecosystem== "human_host-associated" ~ "Human host",
+            ecosystem == "animal_host-associated" ~ "Animal host",
+            ecosystem == "plant_associated" ~ "Plant host",
+            ecosystem == "groundwater" ~ "Groundwater",
+            ecosystem == "freshwater" ~ "Freshwater",
+            ecosystem == "wastewater" ~ "Wastewater",
+            ecosystem == "saline_water" ~ "Saline Water",
+            ecosystem == "sediment" ~ "Sediment",
+            ecosystem == "soil" ~ "Soil"))
+
+di <- di %>%
+      mutate(
+            life_style = case_when(
+              life_style == "host-associated" ~ "Host associated",
+              life_style == "free-living" ~ "Free living"
+            ))
+
+di$ecosystem <- as.factor(di$ecosystem)
+
+#life_style summarized di means
+di_life_style <- di %>%
+      group_by(life_style) %>%
+      dplyr::summarise(
+        n = dplyr::n(),
+        median_mean_di = median(mean_di),
+        mean_mean_di = mean(mean_di),
+        sd_mean_di = sd(mean_di),
+        se_mean_di = sd_mean_di / sqrt(n)) %>%
+      select(-n)
+
+
+#ecosystem summarized di means
+di_ecosystem <- di  %>%
+      group_by(life_style, ecosystem) %>%
+      dplyr::summarise(
+        n = dplyr::n(),
+        median_mean_di = median(mean_di),
+        mean_mean_di = mean(mean_di),
+        sd_mean_di = sd(mean_di),
+        se_mean_di = sd_mean_di / sqrt(n)) %>%
+      select(-n)
+    
+#habitat summarized di means
+di_habitat <- di  %>%
+      group_by(life_style, habitat, ecosystem) %>%
+      dplyr::summarise(
+        n = dplyr::n(),
+        median_mean_di = median(mean_di),
+        mean_mean_di = mean(mean_di),
+        sd_mean_di = sd(mean_di),
+        se_mean_di = sd_mean_di / sqrt(n)) %>%
       select(-n)
 
